8468b9af90fec75eb4263eafa3dcb1b7e06795eb
Emilio <you@example.com>
2021-02-12 21:35:07 +0800

    >

--- p14/p14.py
+++ p14/p14.py
@@ -2,22 +2,44 @@ import sys
 
 sys.path.append('../')
 from util import macro
+from art import logo
+from data import data
+import random
+
+macro.moduleStart("HigherLower")
+
+new_data = []
+a = None
+b = None
+c = None
+def select():
+  x = random.choice(new_data)
+  new_data.remove(x)
+  return x
+def swap():
+  global a, b, c
+  print("Correct!")
+  new_data.append(a)
+  a = b
+  b = c
+  c = select()
 
 def start():
-  p0()
-  # p1()
-  # p2()
+  global a, b, c, new_data
+  new_data = data
+  a = select()
+  b = select()
+  c = select()
+  print(print(logo))
+  correct = True
+  while correct:
+    if input(f"Does {b.get('name')} have more followers than {a.get('name')}? ") == 'yes' or 'y':
+      if b.get('follower_count') > a.get('follower_count'): swap()
+      else: correct = False
+    else:
+      if last.get('follower_count') > first.get('follower_count'): correct = False
+      else: swap()
 
-def p0():
-  macro.moduleStart("")
-  height = float(input("enter your height in m: \n"))
-  weight = float(input("enter your weight in kg: \n"))
-  print(f"Your BMI is: {int(weight / height**2)}")
-
-def p1():
-  macro.moduleStart("")
-
-def p2():
-  macro.moduleStart("")
+  if input("You lose! Would you like to play again? ") == 'yes' or 'y': start()
 
 start()
