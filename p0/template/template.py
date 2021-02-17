3774183c317d47fc2750c748af767d77f36c986b
Emilio <you@example.com>
2021-02-17 22:36:25 +0800

    >

--- p0/template/template.py
+++ p0/template/template.py
@@ -5,12 +5,14 @@ from util import macro
 
 class MenuItem:
     """Models each Menu Item."""
+
     def __init__(self, name, water, milk, coffee, cost):
         self.name = name
         self.cost = cost
-        self.ingredients = {"water": water,
-                            "milk": milk,
-                            "coffee": coffee}
+        self.ingredients = {
+            "water":water,
+            "milk":milk,
+            "coffee":coffee}
 
 class Menu:
     CURRENCY = "$"
@@ -18,8 +20,9 @@ class Menu:
     def __init__(self):
         self.profit = 0
         self.money_received = 0
-        self.menu = [MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
-                     MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5)]
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5)]
 
     def report(self): print(f"Money: {self.CURRENCY}{self.profit}")
 
@@ -39,7 +42,7 @@ def p0():
     macro.moduleStart("")
     height = float(input("enter your height in m: \n"))
     weight = float(input("enter your weight in kg: \n"))
-    print(f"Your BMI is: {int(weight / height ** 2)}")
+    print(f"Your BMI is: {int(weight / height**2)}")
 
 def p1():
     macro.moduleStart("")
